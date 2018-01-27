98ee27e0c2e2711526cd9af54674e44cae480b25
Trim Bresilla <trim.bresilla@gmail.com>
2018-01-27 19:57:56 +0100

    gitignored files

--- collect.py
+++ collect.py
@@ -1,10 +1,11 @@
 import os, keyboard
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 # Labels for image classes and the keyboard event keys associated with them
-label = {"s":"STRAIGHT", "r":"RIGHT", "l":"LEFT"}
+#label = {"s":"STRAIGHT", "r":"RIGHT", "l":"LEFT"}
+label = {"x":"XBOX", "m":"MOUSE", "u":"USB"}
 number = {label[i]:0 for i in label}
 
 # Directory where data will be saved (will be created if does not exist)
@@ -13,7 +14,8 @@ for index, value in enumerate(label):
     if not os.path.exists(data_dir+label[value]):
         os.makedirs(data_dir+label[value])
 
-
+# Number of pictures to be taken per one class
+pic_per_class = 500
 
 while True:
     # Read in single frame from webcam
@@ -24,12 +26,13 @@ while True:
 
     for index, value in enumerate(label):
         if cv2.waitKey(1) & keyboard.is_pressed(value):
-            image_dir = data_dir + label[value] + "/" + str(number[label[value]]) + ".png"
-            frame = cv2.resize(frame,(360,360), interpolation = cv2.INTER_NEAREST)
-            cv2.imwrite(image_dir, frame)
-
-            print image_dir
+            if number[label[value]] <= pic_per_class:
+                image_dir = data_dir + label[value] + "/" + str(number[label[value]]) + ".png"
+                frame = cv2.resize(frame,(360,360), interpolation = cv2.INTER_NEAREST)
+                cv2.imwrite(image_dir, frame)
+                print image_dir
             number[label[value]]+=1
+
 
     # Press ESCAPE to quit the program
     if cv2.waitKey(1) & keyboard.is_pressed("esc"):
