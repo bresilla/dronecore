7f1a39afe751c825c06a32bc73f5b235530ecae9
Trim Bresilla <trim.bresilla@gmail.com>
2018-01-27 01:11:05 +0100

    small changes

--- collect.py
+++ collect.py
@@ -1,47 +1,41 @@
+import os, keyboard
 import cv2
 
-# Run this script from the same directory as your Data folder
-
-# Grab your webcam on local machine
 cap = cv2.VideoCapture(0)
 
-# Give image a name type
 name_type = 'Small_cat'
 
-# Initialize photo count
-number = 0
+label = {"s":"STRAIGHT", "r":"RIGHT", "l":"LEFT"}
+number = {label[i]:0 for i in label}
 
-# Specify the name of the directory that has been premade and be sure that it's the name of your class
-# Remember this directory name serves as your datas label for that particular class
-set_dir = 'Cat'
+data_dir = '/home/trim/Data/'
 
-print ("Photo capture enabled! Press esc to take photos!")
+for index, value in enumerate(label):
+    if not os.path.exists(data_dir+label[value]):
+        os.makedirs(data_dir+label[value])
+
+
 
 while True:
     # Read in single frame from webcam
     ret, frame = cap.read()
 
     # Use this line locally to display the current frame
-    cv2.imshow('Color Picture', frame)
+    cv2.imshow('FRAME', frame)
 
     # Use esc to take photos when you're ready
-    if cv2.waitKey(1) & 0xFF == 27:
+    for index, value in enumerate(label):
+        if cv2.waitKey(1) & keyboard.is_pressed(value):
+            image_dir = data_dir + label[value] + "/" + str(number[label[value]]) + ".png"
+            #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+            frame = cv2.resize(frame,(360,360), interpolation = cv2.INTER_NEAREST)
+            cv2.imwrite(image_dir, frame)
 
-        # If you want them gray
-        #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-
-        # If you want to resize the image
-        # gray_resize = cv2.resize(gray,(360,360), interpolation = cv2.INTER_NEAREST)
-
-        # Save the image
-        cv2.imwrite('Data/' + set_dir + '/' + name_type + "_" + str(number) + ".png", frame)
-
-        print ("Saving image number: " + str(number))
-
-        number+=1
+            print image_dir
+            number[label[value]]+=1
 
     # Press q to quit the program
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & keyboard.is_pressed("esc"):
         break
 
 cap.release()
